91c3a4f089b16211bf36f24034524042ca554cdc
Johanna_Arenas <89162545+JohannaArenas@users.noreply.github.com>
2023-06-23 14:41:30 -0300

    intento de carrito en node js
    intento de carrito en node js

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner-container">
+        <h1>La seguridad con un solo click</h1>
+        <p>Sensor de Puerta</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
